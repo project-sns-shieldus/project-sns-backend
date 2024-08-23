3482ca011059a78d6692176854a44dc5e3243c4a
HyunJong Kim <world2222222222@gmail.com>
2024-08-23 14:52:22 +0900

    ããIDë를 Integer에서 Long으로 변경

--- src/main/java/com/delta/delta/entity/User.java
+++ src/main/java/com/delta/delta/entity/User.java
@@ -17,7 +17,7 @@ import java.util.Set;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer userId;
+    private Long userId;
 
     private String username;
     private String firstname;
